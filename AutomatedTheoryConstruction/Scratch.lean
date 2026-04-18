ac2f15ff8a7b9664b659f58129206df51b9b3615
tukamilano <milanotukamoto@g.ecc.u-tokyo.ac.jp>
2026-04-18 12:51:15 +0900

    update

--- AutomatedTheoryConstruction/Scratch.lean
+++ AutomatedTheoryConstruction/Scratch.lean
@@ -1,13 +1,9 @@
 import Mathlib
 import AutomatedTheoryConstruction.Derived
-import AutomatedTheoryConstruction.Lambek.Notation
 
 set_option autoImplicit false
 
 namespace AutomatedTheoryConstruction
-
-open Mathling.Lambek.ProductFree
-open scoped ATC
 
 -- Temporary Lean code for verification is written here.
 
